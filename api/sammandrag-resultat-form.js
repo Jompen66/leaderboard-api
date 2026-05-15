69ecbb27206031d9efc5d18147d9d69e5c9871ca
Jompen66 <131808228+Jompen66@users.noreply.github.com>
2026-05-15 08:25:17 +0200

    Create sammandrag-resultat-form.js

---
+++ api/sammandrag-resultat-form.js
@@ -0,0 +1,21 @@
+{
+  "eventId": "recEVENTID",
+  "spelform": "Slaggolf",
+  "longestDrivePlayerId": "recPLAYER3",
+  "results": [
+    {
+      "playerId": "recPLAYER1",
+      "strokes": 72,
+      "points": 6,
+      "wineBottles": 2,
+      "nearestPinCount": 1
+    },
+    {
+      "playerId": "recPLAYER2",
+      "strokes": 76,
+      "points": 4,
+      "wineBottles": 0,
+      "nearestPinCount": 2
+    }
+  ]
+}
